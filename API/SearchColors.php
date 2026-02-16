d53ca626ad86c32f814f3ed97e679994d3b0766c
ThatGuyRyan21 <145689869+ThatGuyRyan21@users.noreply.github.com>
2026-02-15 19:51:17 -0500

    Update database connection parameters

--- API/SearchColors.php
+++ API/SearchColors.php
@@ -5,7 +5,7 @@
         $searchResults = "";
         $searchCount = 0;
 
-        $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+        $conn = new mysqli("DB_HOST", "DB_USER", "DB_NAME", "DB_PASS");
         if ($conn->connect_error)
         {
                 returnWithError( $conn->connect_error );
